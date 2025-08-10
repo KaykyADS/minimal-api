c4f6f71303e8ede10afe1e8f130bad4fb8a42931
KaykyADS <kayky.souzacosta@hotmail.com>
2025-08-10 12:06:38 -0300

    Regras para autorização

---
+++ Dominio/Entidades/Administrador.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinimalApi.Dominio.Entidades;
+
+public class Administrador
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(255)]
+    public string Email { get; set; } = default!;
+    [StringLength(50)]
+    [Required]
+    public string Senha { get; set; } = default!;
+    [StringLength(10)]
+    [Required]
+    public string Perfil { get; set; } = default!;     
+}
